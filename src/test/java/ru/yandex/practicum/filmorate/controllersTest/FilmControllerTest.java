5f66b921b2eea2864eec6d564bbfb779065c16e2
Angela Ivleva <mistress2904@yandex.ru>
2023-03-15 11:06:01 +0300

    delete outdated tests

--- src/test/java/ru/yandex/practicum/filmorate/controllersTest/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllersTest/FilmControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controllers.FilmController;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
@@ -49,18 +48,6 @@ public class FilmControllerTest {
         assertEquals(1, filmController.createFilm(film).getId());
     }
 
-    @Test
-    void shouldNotCreateFilmWhenNameIsEmpty() {
-        Film film = Film.builder()
-                .id(0L)
-                .name(" ")
-                .description("The story of a boy who learns on his 11th birthday that he is the orphaned " +
-                        "son of two powerful wizards and possesses unique magical powers of his own.")
-                .releaseDate(LocalDate.of(2001, 11, 16))
-                .duration(152)
-                .build();
-        assertThrows(ValidationException.class, () -> filmController.createFilm(film));
-    }
 
     @Test
     void shouldCreateFilmWhenReleaseDateOnBirthOfCinema() {
@@ -74,50 +61,6 @@ public class FilmControllerTest {
                 .build();
         assertEquals(LocalDate.of(1895, 12, 28),
                 filmController.createFilm(film).getReleaseDate());
-    }
-
-    @Test
-    void shouldNotCreateFilmWhenReleaseDateBeforeBirthOfCinema() {
-        Film film = Film.builder()
-                .id(0L)
-                .name("Harry Potter and the sorcerer's stone")
-                .description("The story of a boy who learns on his 11th birthday that he is the orphaned " +
-                        "son of two powerful wizards and possesses unique magical powers of his own.")
-                .releaseDate(LocalDate.of(1895, 12, 27))
-                .duration(152)
-                .build();
-        assertThrows(ValidationException.class, () -> filmController.createFilm(film));
-    }
-
-    @Test
-    void shouldNotCreateFilmWhenDescriptionLengthIsMore200() {
-        Film film = Film.builder()
-                .id(0L)
-                .name("Harry Potter and the sorcerer's stone")
-                .description("Based on the first of J.K. Rowling's popular children's novels about Harry Potter. " +
-                        "The story of a boy who learns on his 11th birthday that he is the orphaned " +
-                        "son of two powerful wizards and possesses unique magical powers of his own." +
-                        "Invited to attend Hogwarts School of Witchcraft and Wizardry, " +
-                        "Harry embarks on the adventure of a lifetime. " +
-                        "At Hogwarts, he finds the home and the family he has never had.")
-                .releaseDate(LocalDate.of(2001, 11, 16))
-                .duration(152)
-                .build();
-        assertThrows(ValidationException.class, () -> filmController.createFilm(film));
-
-    }
-
-    @Test
-    void shouldNotCreateFilmWhenDurationIsNegative() {
-        Film film = Film.builder()
-                .id(0L)
-                .name("Harry Potter and the sorcerer's stone")
-                .description("The story of a boy who learns on his 11th birthday that he is the orphaned " +
-                        "son of two powerful wizards and possesses unique magical powers of his own.")
-                .releaseDate(LocalDate.of(2001, 11, 16))
-                .duration(-1)
-                .build();
-        assertThrows(ValidationException.class, () -> filmController.createFilm(film));
     }
 
     @Test
